1c378bc654e231b1b6bc352005be4436118a5278
zhangyang <969666310@qq.com>
2019-12-25 11:21:17 +0800

    init commit

---
+++ src/test/java/cn/myfreecloud/javautils/JavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.myfreecloud.javautils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
