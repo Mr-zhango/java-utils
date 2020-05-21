3f2295ee641e7c322d1ea5a65fb6cb8f5453c5ad
zhangyang <zhangyang02@wanxue.cn>
2020-05-21 11:48:10 +0800

    分类信息内存缓存的实现(可以用户友情链接,分类等信息)

---
+++ src/main/java/cn/myfreecloud/javautils/memocache/dto/CatalogDTO.java
@@ -0,0 +1,9 @@
+package cn.myfreecloud.javautils.memocache.dto;
+
+import lombok.Data;
+
+@Data
+public class CatalogDTO {
+    private Integer id;
+    private String name;
+}
---
+++ src/main/java/cn/myfreecloud/javautils/memocache/entity/CatalogEntity.java
@@ -0,0 +1,12 @@
+package cn.myfreecloud.javautils.memocache.entity;
+
+import lombok.Data;
+
+@Data
+public class CatalogEntity {
+    private Integer id;
+
+    private String name;
+
+    private Integer delete;
+}
