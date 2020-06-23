e48670f669386dba9aad91af4ea3f306fb691c76
zhangyang <zhangyang02@wanxue.cn>
2020-06-23 19:41:46 +0800

    add demo

--- demo_mongodb/src/test/java/test/MongodbTest.java
+++ demo_mongodb/src/test/java/test/MongodbTest.java
@@ -5,7 +5,6 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.BasicBSONObject;
 import org.bson.Document;
 import org.junit.Before;
 import org.junit.Test;
